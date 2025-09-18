f86e195f5260f147b7b3aa17794690a1a10ed8d0
Kaushik-exe <kaushik.pk124@gmail.com>
2025-09-18 14:53:45 +0530

    Add src/data/properties.js

---
+++ src/data/properties.js
@@ -0,0 +1,17 @@
+const properties = [
+  {
+    name: 'Property 1',
+    description: 'Description 1',
+    action: 'buy',
+    type: 'flat'
+  },
+  {
+    name: 'Property 2',
+    description: 'Description 2',
+    action: 'rent',
+    type: 'rowhouse'
+  }
+  // Add more mock properties as needed
+];
+
+export default properties;
